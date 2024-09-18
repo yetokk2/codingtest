a27b5dd057399e3732bae409df53105e73da5bd9
신예진 <yjshin@pentasecurity.com>
2024-09-18 14:59:51 +0900

    우선순위 큐와 heap을 공부했다.

--- src/main/java/org/algorithm/programers/heap/Num1.java
+++ src/main/java/org/algorithm/programers/heap/Num1.java
@@ -1,30 +1,24 @@
 package org.algorithm.programers.heap;
 
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.stream.Collectors;
 
 public class Num1 {
     public int solution(int[] scoville, int K) {
         PriorityQueue<Integer> queue = new PriorityQueue<>();
-
         for (int i : scoville) {
             queue.add(i);
         }
-
-        int min1 = queue.poll();
+        
         int count = 0;
-        while (K > min1) {
-            count++;
-            int min2 = queue.poll();
-
-            queue.offer(min1 + (min2 * 2));
+        while (K > queue.peek()) {
             if (queue.size() < 2) {
                 return -1;
             }
-            min1 = queue.poll();
+            int min1 = queue.poll();
+            int min2 = queue.poll();
+
+            count++;
+            queue.offer(min1 + (min2 * 2));
         }
 
         return count;
